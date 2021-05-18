6aeba0695b7fbaf27131087debde54d916d4403d
Dakota Newman <dakota.newman@yahoo.com>
2021-05-18 12:39:23 -0700

    updated assignment 7

--- assignments/Activity7-DakotaNewman.py
+++ assignments/Activity7-DakotaNewman.py
@@ -1,4 +1,24 @@
-def sayHello(name, age):
-  print("Hello, " + name + " and my age is " + age)
+def add(num1, num2):
+  sum = num1 + num2
+  print(sum)
+  return sum
 
-sayHello('Dakota', '28')
+def subtract(num1, num2):
+  difference = num1 - num2
+  print(difference)
+  return difference
+
+def multiply(num1, num2):
+  total = num1 * num2
+  print(total)
+  return total
+
+def divide(num1, num2):
+  result = num1 / num2
+  print(result)
+  return result
+
+add(5, 10)
+subtract(100, 10)
+multiply(7, 5)
+divide(100, 10)
