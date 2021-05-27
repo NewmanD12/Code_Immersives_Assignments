c081895696d49307d6087c2f3da658f27c13a683
Dakota Newman <dakota.newman@yahoo.com>
2021-05-27 09:19:47 -0700

    updated activity 10.x and added activity 10.3

--- assignments/Activity10.3-DakotaNewman.py
+++ assignments/Activity10.3-DakotaNewman.py
@@ -1,35 +1,5 @@
 import random
 
-# Activity 10.1 Create a list of random numbers using getrandbits
-def listOfRandomNums(num):
-  listOfNums = []
-  for i in range(num):
-    randomNum = random.getrandbits(10)
-    listOfNums.append(randomNum)
-  return listOfNums
-
-result = listOfRandomNums(15)
-result2 = listOfRandomNums(1000)
-
-# print(result)
-# print(result2)
-
-# Activity 10.2 create a function that will generate a random list, with the values passed in between 20-40
-
-def listOfRandomNums2(num):
-  listOfNums = []
-  for i in range(num):
-    randomNum = random.randrange(20, 40)
-    listOfNums.append(randomNum)
-  return listOfNums
-
-result3 = listOfRandomNums2(15)
-result4 = listOfRandomNums2(12)
-
-# print(result3)
-# print(result4)
-
-
 # Activity 10.3 Create a function that plays rock paper scissors with you...
 # You are to ask to user for input
 # If the user inputs "Rock" -> "scissors" will lose, and 'Paper' will win
@@ -116,4 +86,3 @@ def rockPaperScissors3():
   # print(computerGuess)
 
 rockPaperScissors3()
-
